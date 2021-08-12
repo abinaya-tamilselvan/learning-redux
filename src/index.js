b0ae59af5bca9a68b63e5b348a8907efc0268b5e
abinaya-tamilselvan <abinaya.tamilselvan@soprasteria.com>
2021-08-12 16:38:13 +0530

    updating

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+const increment = () => {
+  return{
+    type: "INCREMENT"
+  }
+}
+const decrement = () => {
+  return{
+    type: "DECREMENT"
+  }
+}
+const counter =(state = 0,action) =>{
+  switch(action.type){
+     case "INCREMENT":
+       return state+1;
+    case "DECREMENT":
+        return state-1;
+  }
+}
+let store = createStore(counter)
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +39,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
