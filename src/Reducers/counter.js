59d6a43af35c59633dc04a4f5be744958c019931
abinaya-tamilselvan <abinaya.tamilselvan@soprasteria.com>
2021-08-12 20:01:35 +0530

    updated counter.js file

--- src/Reducers/counter.js
+++ src/Reducers/counter.js
@@ -1,10 +1,13 @@
-const CounterReducer = (state=0,action) =>{
-    switch(action.type){
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
         case "INCREMENT":
-            return state+1
-       case "DECREMENT":
-            return state-1
-       default
+            return state + 1
+        case "DECREMENT":
+            return state - 1
+        default:
+            return state
     }
-};
-export default CounterReducer;
+
+}
+
+export default counterReducer
