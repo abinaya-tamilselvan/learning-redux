3102fa581b376a1a9e05c648a26e91ed763d83c1
abinaya-tamilselvan <abinaya.tamilselvan@soprasteria.com>
2021-08-12 20:11:25 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 
-import './App.css';
 import React from "react";
-import {useSelector} from "react-redux";
-import {increment} from "./Actions";
+import { useSelector,useDispatch } from "react-redux";
+import {increment,decrement} from "./Actions";
 
 function App() {
-  const counter =useSelector(state =>state.counter);
-  const isLogged = useSelector(state =>state.isLogged);
+const counter = useSelector(state => state.counter)
+const isLogged = useSelector(state => state.isLogged)
+const dispatch = useDispatch();
+// we are using redux bcoz we dont hv to import everything in app file , we automatically call this will same time and space
+
   return (
     <>
-    <div className="App">
-         <h1>counter</h1>
-         <button onClick ={()=>dispatchEvent(increment())}>+ </button>
-         <button>+ </button>
-         {isLogged ? <h3> for authorized person only</h3>: " "}
-    </div>
+      <div className="App">
+        <h1>Counter {counter}</h1>
+        <button onClick={() =>dispatch(increment())}>+</button>
+        <button onClick={() =>dispatch(decrement())}>-</button>
+        {isLogged ? <h3>for authorized person only</h3> : " "}
+
+      </div>
     </>
   );
 }
