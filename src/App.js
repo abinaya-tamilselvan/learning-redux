7383771b209da1379b825cfba2c0aee8712c29df
abinaya-tamilselvan <abinaya.tamilselvan@soprasteria.com>
2021-08-12 19:57:11 +0530

    combineReducers in Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import React from "react";
+import {useSelector} from "react-redux";
+import {increment} from "./Actions";
 
 function App() {
+  const counter =useSelector(state =>state.counter);
+  const isLogged = useSelector(state =>state.isLogged);
   return (
+    <>
     <div className="App">
-      
+         <h1>counter</h1>
+         <button onClick ={()=>dispatchEvent(increment())}>+ </button>
+         <button>+ </button>
+         {isLogged ? <h3> for authorized person only</h3>: " "}
     </div>
+    </>
   );
 }
 
